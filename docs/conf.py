c5c10a046645b36297c1def702763ef3773b5f9f
j.borbely <joe.borbely@gmail.com>
2017-05-10 16:58:32 +1200

    use the 'mock' package for building docs on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -9,6 +9,12 @@ sys.path.insert(0, os.path.abspath('..'))
 
 from msl import equipment
 
+import mock
+
+MOCK_MODULES = ['msl.loadlib', 'numpy', 'pyvisa', 'xlrd']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.

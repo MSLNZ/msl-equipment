e5771017a94b4361bc9c255be52b595270a66df5
j.borbely <joe.borbely@gmail.com>
2017-06-02 16:26:37 +1200

    remove mocking

--- docs/conf.py
+++ docs/conf.py
@@ -12,15 +12,15 @@ on_rtd = os.environ.get('READTHEDOCS') == 'True'
 if on_rtd:
     html_theme = 'default'
 
-    from unittest.mock import MagicMock
-
-    class Mock(MagicMock):
-        @classmethod
-        def __getattr__(cls, name):
-            return MagicMock()
-
-    MOCK_MODULES = ['ctypes', 'numpy', 'numpy.ctypeslib']
-    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+    # from unittest.mock import MagicMock
+    #
+    # class Mock(MagicMock):
+    #     @classmethod
+    #     def __getattr__(cls, name):
+    #         return MagicMock()
+    #
+    # MOCK_MODULES = ['ctypes', 'numpy', 'numpy.ctypeslib']
+    # sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 else:
     html_theme = 'sphinx_rtd_theme'

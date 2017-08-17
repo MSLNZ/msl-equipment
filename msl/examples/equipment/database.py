0b86510e96a772c7efbfbbe24bc1992e23fd6c72
j.borbely <joe.borbely@gmail.com>
2017-08-17 14:15:09 +1200

    print empty string

--- msl/examples/equipment/database.py
+++ msl/examples/equipment/database.py
@@ -17,19 +17,19 @@ if __name__ == '__main__':
     print(dbase.equipment['sensor'])
     print(dbase.equipment['adaptor'])
     print(dbase.equipment['filter_flipper'])
-    print()
+    print('')
 
     # search for equipment records based on some keywords
     print('All equipment in the database manufactured by Hewlett Packard...')
     for record in dbase.records(manufacturer='H*P'):
         print(record)
-    print()
+    print('')
 
     # show all of the connection records in the database
     print('All connection records in the database that use GPIB...')
     for record in dbase.connections(address='GPIB*'):
         print(repr(record))
-    print()
+    print('')
 
     # get a specific equipment record (a DMM from Agilent) from the database and
     # then connect to this DMM in demo mode
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     dmm_record = dbase.records(manufacturer='Agilent', serial='G00001')
     dmm_ref = connect(dmm_record, demo=True)  # use the factory function to connect to the equipment
     print(dmm_ref.query('*IDN?'))
-    print()
+    print('')
 
     # connect to the <equipment> XML elements that are listed in the config file
     print('Connect to the equipment in a for loop...')
@@ -49,7 +49,7 @@ if __name__ == '__main__':
             continue
         conns[key] = equip.connect(demo=True)  # use the EquipmentRecord object to connect to the equipment
     print(conns)
-    print()
+    print('')
 
     print("Ask the 'ref' DMM for its identity...")
     print(conns['ref'].ask('*IDN?'))  # 'ask' is an alias for the 'query' method

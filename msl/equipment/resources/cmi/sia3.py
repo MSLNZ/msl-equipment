ccc4e9b360df390096d5b11b1ee8d5efa6c513a6
jborbely <joe.borbely@gmail.com>
2017-08-26 17:33:24 +1200

    define default values for ConnectionRecord.properties

--- msl/equipment/resources/cmi/sia3.py
+++ msl/equipment/resources/cmi/sia3.py
@@ -1,10 +1,17 @@
+"""
+Establishes a connection to the Switched Integrator Amplifier
+(SIA3 board) that is designed by the `Czech Metrology Institute`_.
+
+.. _Czech Metrology Institute: https://www.cmi.cz/?language=en
+"""
 from enum import IntEnum
 
+from msl.equipment import constants
 from msl.equipment.connection_msl import ConnectionSerial
 
 
 class IntegrationTime(IntEnum):
-    """The amount of time to integrate the photodiode signal."""
+    """The amount of time to integrate the photo-diode signal."""
     TIME_50u = 5
     TIME_100u = 6
     TIME_1m = 7
@@ -24,26 +31,42 @@ class SIA3(ConnectionSerial):
     def __init__(self, record):
         """
         Establishes a connection to the Switched Integrator Amplifier
-        (SIA3 board) that is designed by the Czech Metrology Institute.
+        (SIA3 board) that is designed by the `Czech Metrology Institute`_.
+
+        Do not instantiate this class directly. Use the :meth:`~.EquipmentRecord.connect`
+        method to connect to the equipment.
+
+        .. _Czech Metrology Institute: https://www.cmi.cz/?language=en
 
         Parameters
         ----------
-        record : :class:`~msl.equipment.record_types.EquipmentRecord`
-            An equipment record from an **Equipment-Register**
-            :class:`~msl.equipment.database.Database`.
+        record : :class:`~.EquipmentRecord`
+            A record from an :ref:`equipment_database`.
         """
+        if len(record.connection.properties) == 0:
+            # then use the default connection properties
+            record.connection.properties = {
+                'baud_rate': 14400,
+                'data_bits': constants.DataBits.EIGHT,
+                'parity': constants.Parity.NONE,
+                'stop_bits': constants.StopBits.ONE,
+                'write_timeout': 10.0,
+                'xon_xoff': False,
+                'rts_cts': False,
+                'dsr_dtr': False
+            }
         ConnectionSerial.__init__(self, record)
 
-    def set_integration_time(self, t):
+    def set_integration_time(self, time):
         """Set the integration time (i.e., the gain).
 
         Parameters
         ----------
-        t : :class:`.IntegrationTime`
+        time : :class:`.IntegrationTime`
             The integration time as a :class:`.IntegrationTime` enum value or member name.
         """
         self._send_byte(7)
-        self._send_byte(self.convert_to_enum(t, IntegrationTime, prefix='TIME_'))
+        self._send_byte(self.convert_to_enum(time, IntegrationTime, prefix='TIME_'))
 
     def set_ps(self, ps):
         """Set the PS.
@@ -51,7 +74,7 @@ class SIA3(ConnectionSerial):
         Parameters
         ----------
         ps : int
-            The PS to use. Value must be in the range [0, 7].
+            The PS to use. The value must be in the range [0, 7].
 
         Raises
         ------
@@ -60,7 +83,7 @@ class SIA3(ConnectionSerial):
         """
         _ps = int(ps)
         if _ps < 0 or _ps > 7:
-            raise ValueError('Invalid ps of {}. Must be 0 <= ps <= 7.'.format(ps))
+            raise ValueError('Invalid PS value of {}. Must be 0 <= PS <= 7.'.format(ps))
         self._send_byte(1)
         self._send_byte(4)
         self._send_byte(_ps)
@@ -68,4 +91,3 @@ class SIA3(ConnectionSerial):
 
     def _send_byte(self, byte):
         self.write(chr(byte))
-

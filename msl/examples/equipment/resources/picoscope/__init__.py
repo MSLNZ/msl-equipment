39f51157631c5605128dac8260332065a7e29b83
jborbely <joe.borbely@gmail.com>
2017-11-24 13:15:20 +1300

    include more comments for the PicoScope EquipmentRecord

--- msl/examples/equipment/resources/picoscope/__init__.py
+++ msl/examples/equipment/resources/picoscope/__init__.py
@@ -6,11 +6,11 @@ from msl.equipment import EquipmentRecord, ConnectionRecord, Backend
 
 record = EquipmentRecord(
     manufacturer='Pico Technology',
-    model='5244B',
-    serial='DY135/055',
+    model='5244B',  # must update the model number for your PicoScope
+    serial='DY135/055',  # must update the serial number for your PicoScope
     connection=ConnectionRecord(
         backend=Backend.MSL,
-        address='SDK::PicoScope5000A::ps5000a',
+        address='SDK::PicoScope5000A::ps5000a',  # "..\Pico Technology\SDK\lib\" must be in the environment PATH
         properties={
             'resolution': '14bit',  # only used for ps5000a series PicoScope's
             'auto_select_power': True,  # for PicoScopes that can be powered by an AC adaptor or by a USB cable

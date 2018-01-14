66fedce733b2bae10d31e428e177eaaa6c6990d7
jborbely <joe.borbely@gmail.com>
2018-01-15 10:52:10 +1300

    update copyright to 2018

--- msl/equipment/__init__.py
+++ msl/equipment/__init__.py
@@ -8,7 +8,7 @@ from msl.equipment.record_types import EquipmentRecord, ConnectionRecord
 from msl.equipment.constants import Backend
 
 __author__ = 'Joseph Borbely'
-__copyright__ = '\xa9 2017, ' + __author__
+__copyright__ = '\xa9 2017 - 2018, ' + __author__
 __version__ = '0.1.0'
 
 version_info = namedtuple('version_info', 'major minor micro')(*map(int, __version__.split('.')[:3]))

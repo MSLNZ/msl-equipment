3e1dcdfd1b611fde567a1172974356477c420707
jborbely <joe.borbely@gmail.com>
2018-05-30 16:51:51 +1200

    fix that the MotionControlCallback example was not showing in docstring

--- msl/equipment/resources/thorlabs/kinesis/callbacks.py
+++ msl/equipment/resources/thorlabs/kinesis/callbacks.py
@@ -1,5 +1,27 @@
 """
-Callbacks defined in the Thorlabs Kinesis software v1.11.0
+A callback to register for a :class:`~msl.equipment.resources.thorlabs.kinesis.motion_control.MotionControl`
+message queue.
+
+.. code-block:: python
+
+   from msl.equipment import Config
+   from msl.equipment.resources.thorlabs import MotionControlCallback
+   from msl.examples.equipment import EXAMPLES_DIR
+
+   @MotionControlCallback
+   def msg_callback():
+       print('MotionControlCallback: ', flipper.convert_message(*flipper.get_next_message()))
+
+   # The "equipment-configuration.xml" configuration file contains the following element:
+   # <equipment alias="filter_flipper" manufacturer="Thorlabs" model="MFF101/M" serial="37871232"/>
+
+   db = Config(os.path.join(EXAMPLES_DIR, 'equipment-configuration.xml')).database()
+
+   flipper = db.equipment['filter_flipper'].connect()
+   flipper.register_message_callback(msg_callback)
+
+   ... do stuff with the `flipper` ...
+
 """
 from msl.loadlib import IS_WINDOWS
 
@@ -11,24 +33,7 @@ else:
     FUNCTYPE = CFUNCTYPE
 
 MotionControlCallback = FUNCTYPE(None)
-"""A callback to register for a MotionControl message queue.
-
-Example usage::
-
-    from msl.equipment import Config
-    from msl.equipment.resources.thorlabs import MotionControlCallback
-    from msl.examples.equipment import EXAMPLES_DIR
-    
-    @MotionControlCallback
-    def msg_callback():
-        print('MotionControlCallback: ', flipper.convert_message(*flipper.get_next_message()))        
-
-    # The "equipment-configuration.xml" configuration file contains the following element:
-    # <equipment alias="filter_flipper" manufacturer="Thorlabs" model="MFF101/M" serial="37871232"/>
-
-    db = Config(os.path.join(EXAMPLES_DIR, 'equipment-configuration.xml')).database()
-    flipper = db.equipment['filter_flipper'].connect()
-    flipper.register_message_callback(msg_callback)
-    
-    ... do stuff with the `flipper` ...
+"""
+A callback to register for a :class:`~msl.equipment.resources.thorlabs.kinesis.motion_control.MotionControl`
+message queue.
 """

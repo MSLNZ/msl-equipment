9a38b599d7fd847ece180ea9cdf8824e19c847fd
jborbely <joe.borbely@gmail.com>
2018-12-14 17:16:22 +1300

    add PicoTech's SDK path to sys.path

--- msl/examples/equipment/resources/picotech/picoscope/find_picoscopes.py
+++ msl/examples/equipment/resources/picotech/picoscope/find_picoscopes.py
@@ -4,8 +4,12 @@ This example prints a list of all the PicoScopes that are connected to the compu
 
 # this "if" statement is used so that Sphinx does not execute this script when the docs are being built
 if __name__ == '__main__':
+    import sys
 
     from msl.equipment.resources.picotech.picoscope import enumerate_units
+
+    # ensure that the PicoTech DLLs are available on PATH
+    sys.path.append(r'C:\Program Files\Pico Technology\SDK\lib')
 
     units = enumerate_units()
     print('The following serial numbers were found: {}'.format(units))

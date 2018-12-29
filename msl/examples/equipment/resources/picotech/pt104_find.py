485cca7b100845eb8338edfb01d641feab190133
jborbely <joe.borbely@gmail.com>
2018-12-30 12:11:52 +1300

    add the PicoTech SDK to os.environ['PATH'] instead of sys.path

--- msl/examples/equipment/resources/picotech/pt104_find.py
+++ msl/examples/equipment/resources/picotech/pt104_find.py
@@ -4,11 +4,11 @@ Example showing how to list all Pico Technology PT-104 Platinum Resistance Data 
 
 # this "if" statement is used so that Sphinx does not execute this script when the docs are being built
 if __name__ == '__main__':
-    import sys
+    import os
     from msl.equipment.resources.picotech.pt104 import enumerate_units
 
     # ensure that the PicoTech DLLs are available on PATH
-    sys.path.append(r'C:\Program Files\Pico Technology\SDK\lib')
+    os.environ['PATH'] += os.pathsep + r'C:\Program Files\Pico Technology\SDK\lib'
 
     units = enumerate_units()
-    print('Found the following PT-104\'s: {}'.format(units))
+    print('The following PT-104\'s were found: {}'.format(units))

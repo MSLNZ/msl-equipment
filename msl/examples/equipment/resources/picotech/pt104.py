43cf24b3302b83121b2f02dac4c59d995d36a5d7
jborbely <joe.borbely@gmail.com>
2019-06-06 11:03:23 +1200

    include a PT100 channel for the PicoTech PT-104 example

--- msl/examples/equipment/resources/picotech/pt104.py
+++ msl/examples/equipment/resources/picotech/pt104.py
@@ -44,13 +44,23 @@ if __name__ == '__main__':
     # configure channel 1 to be single-ended voltage from 0 to 2.5 V
     pt104.set_channel(1, pt104.DataType.SINGLE_ENDED_TO_2500MV, 2)
 
-    for i in range(10):
-        # wait for samples to be available
-        # measurement takes about 1 second per active channel
-        time.sleep(1.5)
+    # configure channel 2 to be a 4-wire PRT, PT100
+    pt104.set_channel(2, pt104.DataType.PT100, 4)
 
-        # read the value at channel 1
-        value = pt104.get_value(1)
+    for i in range(10):
+        # Wait for the samples to be available
+        # A measurement cycle takes about 1 second per active channel
+        # 2 channels are active, so we should wait at least 2 seconds
+        time.sleep(3)
+
+        # read the value of channel 1
+        ch1 = pt104.get_value(1)
+
+        # read the value of channel 2
+        ch2 = pt104.get_value(2)
 
         # for the SINGLE_ENDED_TO_2500MV configuration the scaling factor is 10 nV
-        print('Loop {}, Voltage {}'.format(i, value * 10e-9))
+        print('Loop {}, Voltage {}'.format(i, ch1 * 10e-9))
+
+        # for the PT100 configuration the scaling factor is 1/1000 deg C
+        print('Loop {}, Temperature {}'.format(i, ch2 * 1e-3))

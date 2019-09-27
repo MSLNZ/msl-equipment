d78ddf725cab919f4972c3ff846804704053e8ac
jborbely <joe.borbely@gmail.com>
2019-09-27 13:17:03 +1200

    update the wait() function in the kdc101, kst101 and lts150 examples

--- msl/examples/equipment/resources/thorlabs/kst101.py
+++ msl/examples/equipment/resources/thorlabs/kst101.py
@@ -24,14 +24,15 @@ if __name__ == '__main__':
         ),
     )
 
-    def wait(value):
+    def wait():
         motor.clear_message_queue()
-        message_type, message_id, _ = motor.wait_for_message()
-        while message_type != 2 or message_id != value:
+        while True:
+            status = motor.convert_message(*motor.wait_for_message())['id']
+            if status == 'Homed' or status == 'Moved':
+                break
             position = motor.get_position()
             real = motor.get_real_value_from_device_unit(position, 'DISTANCE')
-            print('  at position {} [device units] {} [real-world units]'.format(position, real))
-            message_type, message_id, _ = motor.wait_for_message()
+            print('  at position {} [device units] {:.3f} [real-world units]'.format(position, real))
 
     # avoid the FT_DeviceNotFound error
     MotionControl.build_device_list()
@@ -49,25 +50,25 @@ if __name__ == '__main__':
     # home the device
     print('Homing...')
     motor.home()
-    wait(0)
+    wait()
     print('Homing done. At position {} [device units]'.format(motor.get_position()))
 
     # move to position 100000
     print('Moving to 100000...')
     motor.move_to_position(100000)
-    wait(1)
+    wait()
     print('Moving done. At position {} [device units]'.format(motor.get_position()))
 
     # move by a relative amount of -5000
     print('Moving by -5000...')
     motor.move_relative(-5000)
-    wait(1)
+    wait()
     print('Moving done. At position {} [device units]'.format(motor.get_position()))
 
     # jog forwards
     print('Jogging forwards by {} [device units]'.format(motor.get_jog_step_size()))
     motor.move_jog('Forwards')
-    wait(1)
+    wait()
     print('Jogging done. At position {} [device units]'.format(motor.get_position()))
 
     # stop polling and close the connection
--- msl/examples/equipment/resources/thorlabs/lts150.py
+++ msl/examples/equipment/resources/thorlabs/lts150.py
@@ -30,13 +30,15 @@ if __name__ == '__main__':
         ),
     )
 
-    def wait(value):
+    def wait():
         motor.clear_message_queue()
-        message_type, message_id, _ = motor.wait_for_message()
-        while message_type != 2 or message_id != value:
+        while True:
+            status = motor.convert_message(*motor.wait_for_message())['id']
+            if status == 'Homed' or status == 'Moved':
+                break
             position = motor.get_position()
-            print('  at position {} [device units]'.format(position))
-            message_type, message_id, _ = motor.wait_for_message()
+            real = motor.get_real_value_from_device_unit(position, 'DISTANCE')
+            print('  at position {} [device units] {:.3f} [real-world units]'.format(position, real))
 
     # Build the device list before connecting to the Integrated Stepper Motor
     MotionControl.build_device_list()
@@ -45,31 +47,35 @@ if __name__ == '__main__':
     motor = record.connect()
     print('Connected to {}'.format(motor))
 
+    # initialize
+    motor.enable_channel()
+    motor.load_settings()
+
     # start polling at 200 ms
     motor.start_polling(200)
 
     # home the device
     print('Homing...')
     motor.home()
-    wait(0)
+    wait()
     print('Homing done. At position {} [device units]'.format(motor.get_position()))
 
     # move to position 100000
     print('Moving to 100000...')
     motor.move_to_position(100000)
-    wait(1)
+    wait()
     print('Moving done. At position {} [device units]'.format(motor.get_position()))
 
     # move by a relative amount of -5000
     print('Moving by -5000...')
     motor.move_relative(-5000)
-    wait(1)
+    wait()
     print('Moving done. At position {} [device units]'.format(motor.get_position()))
 
     # jog forwards
     print('Jogging forwards by {} [device units]'.format(motor.get_jog_step_size()))
     motor.move_jog('Forwards')
-    wait(1)
+    wait()
     print('Jogging done. At position {} [device units]'.format(motor.get_position()))
 
     # stop polling and close the connection

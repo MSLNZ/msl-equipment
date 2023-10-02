ab336e0c8f358cdf987a8f10c47e0c706e5e6154
jborbely <joe.borbely@gmail.com>
2023-10-03 12:24:07 +1300

    use sphinx_rtd_theme on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
+    'sphinx_rtd_theme',
 ]
 
 # autodoc options
@@ -110,11 +111,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-on_rtd = os.getenv('READTHEDOCS') == 'True'
-if on_rtd:
-    html_theme = 'default'
-else:
-    html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

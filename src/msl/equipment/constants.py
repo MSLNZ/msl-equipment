8c95cb38ce69fd206326d450e3e26a2cdde070e9
jborbely <joe.borbely@gmail.com>
2025-08-31 17:52:20 +1200

    fmt/lint constants.py

--- src/msl/equipment/constants.py
+++ src/msl/equipment/constants.py
@@ -1,72 +1,46 @@
-"""
-MSL-Equipment constants.
-"""
+"""Constants used by `msl-equipment`."""
+
 from __future__ import annotations
 
-import datetime
 import enum
 import re
 import sys
 
 import serial
 
-CR = b'\r'
-LF = b'\n'
+CR = b"\r"
+LF = b"\n"
 
-IS_LINUX: bool = sys.platform == 'linux'
-IS_WINDOWS: bool = sys.platform == 'win32'
+IS_LINUX: bool = sys.platform == "linux"
+IS_WINDOWS: bool = sys.platform == "win32"
 
-REGEX_SDK = re.compile(
-    r'SDK::(?P<path>.+)',
-    flags=re.IGNORECASE
-)
+REGEX_SDK = re.compile(r"SDK::(?P<path>.+)", flags=re.IGNORECASE)
 
-REGEX_SERIAL = re.compile(
-    r'(COM|ASRL|ASRLCOM)((?P<dev>/dev/[^\s:]+)|(?P<number>\d+))',
-    flags=re.IGNORECASE
-)
+REGEX_SERIAL = re.compile(r"(COM|ASRL|ASRLCOM)((?P<dev>/dev/[^\s:]+)|(?P<number>\d+))", flags=re.IGNORECASE)
 
 REGEX_SOCKET = re.compile(
-    r'(?P<prefix>SOCKET|TCP|UDP|TCPIP\d*)::(?P<host>[^\s:]+)::(?P<port>\d+)(?P<suffix>::SOCKET)?',
-    flags=re.IGNORECASE
+    r"(?P<prefix>SOCKET|TCP|UDP|TCPIP\d*)::(?P<host>[^\s:]+)::(?P<port>\d+)(?P<suffix>::SOCKET)?", flags=re.IGNORECASE
 )
 
 # TCPIP[board]::host address[::LAN device name][::INSTR]
 REGEX_TCPIP = re.compile(
-    r'TCPIP(?P<board>\d*)'
-    r'::(?P<host>[^\s:]+)'
-    r'(::(?P<name>([^\s:]+\d+(\[.+])?)))?'
-    r'(::INSTR)?$',
-    flags=re.IGNORECASE
+    r"TCPIP(?P<board>\d*)::(?P<host>[^\s:]+)(::(?P<name>([^\s:]+\d+(\[.+])?)))?(::INSTR)?$", flags=re.IGNORECASE
 )
 
 REGEX_PROLOGIX = re.compile(
-    r'Prologix::'
-    r'(?P<name>[^\s:]+)'
-    r'(?P<port>::1234)?'
-    r'(::GPIB\d*)?'
-    r'::(?P<pad>\d+)'
-    r'(::(?P<sad>\d+))?',
-    flags=re.IGNORECASE
+    r"Prologix::(?P<name>[^\s:]+)(?P<port>::1234)?(::GPIB\d*)?::(?P<pad>\d+)(::(?P<sad>\d+))?", flags=re.IGNORECASE
 )
 
-REGEX_ZMQ = re.compile(
-    r'ZMQ::(?P<host>[^\s:]+)::(?P<port>\d+)',
-    flags=re.IGNORECASE
-)
+REGEX_ZMQ = re.compile(r"ZMQ::(?P<host>[^\s:]+)::(?P<port>\d+)", flags=re.IGNORECASE)
 
 REGEX_GPIB = re.compile(
-    r'GPIB(?P<board>\d{0,2})(::((?P<pad>\d+)|(?P<name>[^\s:]+)))?(::(?P<sad>\d+))?',
-    flags=re.IGNORECASE
+    r"(?P<board>\d{0,2})(::((?P<pad>\d+)|(?P<name>[^\s:]+)))?(::(?P<sad>\d+))?", flags=re.IGNORECASE
 )
-
-DEFAULT_DATE = datetime.date(datetime.MINYEAR, 1, 1)
 
 
 class Backend(enum.IntEnum):
-    """
-    The software backend to use for the communication system.
-    """
+    """The software backend to use for the communication system."""
+
     UNKNOWN = 0
     MSL = 1
     PyVISA = 2
@@ -74,12 +48,11 @@ class Backend(enum.IntEnum):
 
 
 class Interface(enum.IntEnum):
+    """The interface to use for the communication system.
+
+    Only used if [Backend.MSL][msl.equipment.constants.Backend.MSL] is chosen as the backend.
     """
-    The interface to use for the communication system that transfers data between
-    a computer and the equipment. Only used if
-    :data:`Backend.MSL <msl.equipment.constants.Backend.MSL>` is chosen
-    as the backend.
-    """
+
     NONE = 0
     SDK = 1
     SERIAL = 2
@@ -92,9 +65,8 @@ class Interface(enum.IntEnum):
 
 
 class Parity(enum.Enum):
-    """
-    The parity type to use for Serial communication.
-    """
+    """The parity type to use for Serial communication."""
+
     NONE = serial.PARITY_NONE
     ODD = serial.PARITY_ODD
     EVEN = serial.PARITY_EVEN
@@ -103,18 +75,16 @@ class Parity(enum.Enum):
 
 
 class StopBits(enum.Enum):
-    """
-    The number of stop bits to use for Serial communication.
-    """
+    """The number of stop bits to use for Serial communication."""
+
     ONE = serial.STOPBITS_ONE
     ONE_POINT_FIVE = serial.STOPBITS_ONE_POINT_FIVE
     TWO = serial.STOPBITS_TWO
 
 
 class DataBits(enum.IntEnum):
-    """
-    The number of data bits to use for Serial communication.
-    """
+    """The number of data bits to use for Serial communication."""
+
     FIVE = serial.FIVEBITS
     SIX = serial.SIXBITS
     SEVEN = serial.SEVENBITS

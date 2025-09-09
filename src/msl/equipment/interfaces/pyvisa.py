b772773adbc1754795c5d71eca171dfac3e36494
jborbely <joe.borbely@gmail.com>
2025-09-09 16:43:18 +1200

    simplify pyvisa interface

--- src/msl/equipment/interfaces/pyvisa.py
+++ src/msl/equipment/interfaces/pyvisa.py
@@ -9,7 +9,7 @@ from typing import TYPE_CHECKING
 
 try:
     import pyvisa
-except ImportError:
+except ImportError:  # pragma: no branch
     pyvisa = None  # type: ignore[assignment]
 
 from msl.equipment.enumerations import Backend, Parity, StopBits
@@ -17,37 +17,41 @@ from msl.equipment.schema import Interface
 from msl.equipment.utils import logger, to_enum
 
 if TYPE_CHECKING:
-    from typing import Any, ClassVar
+    from typing import Any
 
-    from pyvisa.highlevel import ResourceManager, VisaLibraryBase
+    from pyvisa.highlevel import ResourceManager
     from pyvisa.resources.resource import Resource
 
-    from msl.equipment.schema import Connection, Equipment
+    from msl.equipment.schema import Equipment
 
 
 class PyVISA(Interface, backend=Backend.PyVISA):
-    """Use [PyVISA]{:target="_blank"} as the backend to communicate with the equipment.
+    """Use [PyVISA](https://pyvisa.readthedocs.io/en/stable/) as the backend to communicate with the equipment."""
 
-    [PyVISA]: https://pyvisa.readthedocs.io/en/stable/
-    """
+    rm: ResourceManager | None = None
+    """PyVISA Resource Manager."""
 
-    _resource_classes: ClassVar[dict[str, type[Resource]]] = {}
-
-    def __init__(self, equipment: Equipment) -> None:  # noqa: C901
+    def __init__(self, equipment: Equipment) -> None:
         """Use [PyVISA] as the backend to communicate with the equipment.
 
         The [backend][msl.equipment.connections.Connection.backend]
         value must be equal to `PyVISA` to use this class for the communication backend.
 
-        The returned object from calling the [connect][msl.equipment.schema.Equipment.connect]
-        method is equivalent to calling
-        [open_resource][pyvisa.highlevel.ResourceManager.open_resource]{:target="_blank"}, e.g.,
+        The `PYVISA_LIBRARY` environment variable is used (if it exists) to create the
+        [ResourceManager][pyvisa.highlevel.ResourceManager]. This environment variable
+        can be defined in a [configuration file][config-xml-example] or by defining the
+        environment variable in your code before connecting to the equipment using
+        [PyVISA][msl.equipment.interfaces.pyvisa.PyVISA] for the first time. The default
+        value is `@ivi` if this environment variable is not defined.
+
+        The returned object using `msl-equipment` to connect to the equipment is equivalent
+        to calling [open_resource][pyvisa.highlevel.ResourceManager.open_resource], e.g.,
 
         ```python
-        from msl.equipment import Backend, Connection, Equipment
+        from msl.equipment import Backend, Connection
 
-        equipment = Equipment(connection=Connection(address="GPIB::12", backend=Backend.PyVISA))
-        inst = equipment.connect()
+        connection = Connection("GPIB::12", backend=Backend.PyVISA)
+        inst = connection.connect()
         print(inst.query("*IDN?"))
         ```
 
@@ -62,7 +66,9 @@ class PyVISA(Interface, backend=Backend.PyVISA):
         ```
 
         You can also combine the packages, use `msl-equipment` for managing information
-        about the equipment and directly use `pyvisa` for the connection
+        about the equipment and directly use `pyvisa` for the connection. If you use this
+        method, the editor you use to develop your code will have better support for
+        features like code completion and type checking.
 
         ```python
         import pyvisa
@@ -71,7 +77,6 @@ class PyVISA(Interface, backend=Backend.PyVISA):
         # config.xml contains <equipment eid="MSLE.0.063" name="dmm"/>
         # and specifies where the equipment registers are and the connections file.
         cfg = Config("config.xml")
-
         address = cfg.equipment["dmm"].connection.address
 
         rm = pyvisa.ResourceManager()
@@ -87,66 +92,21 @@ class PyVISA(Interface, backend=Backend.PyVISA):
         self._resource: Resource | None = None
         super().__init__(equipment)
 
-        rm: ResourceManager = PyVISA.resource_manager()
+        if pyvisa is None:  # pragma: no branch
+            msg = "pyvisa is not installed, run: pip install pyvisa"  # type: ignore[unreachable]
+            raise RuntimeError(msg)
 
         assert equipment.connection is not None  # noqa: S101
-        assert pyvisa is not None  # noqa: S101
+        kwargs = _prepare_kwargs(equipment.connection.properties)
 
-        props = equipment.connection.properties
+        if PyVISA.rm is None:
+            PyVISA.rm = pyvisa.ResourceManager()
 
-        # In PyVISA, Parity and StopBits must be integers
-        if "parity" in props:
-            parity = props["parity"]
-            if isinstance(parity, str):
-                parity = to_enum(parity, Parity, to_upper=True)
-            if isinstance(parity, Parity):
-                parity = parity.name.lower()
-                props["parity"] = to_enum(parity, pyvisa.constants.Parity)  # pyright: ignore[reportUnknownArgumentType, reportUnknownMemberType, reportAttributeAccessIssue]
-
-        if "stop_bits" in props:
-            stop_bits = props["stop_bits"]
-            if isinstance(stop_bits, StopBits):
-                stop_bits = int(stop_bits.value * 10)
-            elif isinstance(stop_bits, (int, float)) and stop_bits < 10:  # noqa: PLR2004
-                stop_bits = int(stop_bits * 10)
-            props["stop_bits"] = to_enum(stop_bits, pyvisa.constants.StopBits)  # pyright: ignore[reportUnknownArgumentType, reportUnknownMemberType, reportAttributeAccessIssue]
-
-        # PyVISA requires the read/write termination data type to be str not bytes
-        def ensure_term(term: bytes) -> str | bytes:
-            try:
-                return term.decode()
-            except AttributeError:
-                return term
-
-        # "termination" is a shortcut used by the MSL backend to set both
-        # write_termination and read_termination to the same value
-        rw_term = props.pop("termination", None)
-        r_term = props.pop("read_termination", rw_term)
-        w_term = props.pop("write_termination", rw_term)
-        if r_term is not None:
-            props["read_termination"] = ensure_term(r_term)
-        if w_term is not None:
-            props["write_termination"] = ensure_term(w_term)
-
-        # the "timeout" value is in seconds for MSL backend
-        # PyVISA uses a timeout in milliseconds
-        timeout: float | None = props.get("timeout")
-        if timeout and timeout < 100:  # noqa: PLR2004
-            # if timeout < 100 then it's value is probably in seconds
-            props["timeout"] = timeout * 1000
-
-        self._resource = rm.open_resource(equipment.connection.address, **props)
+        self._resource = PyVISA.rm.open_resource(equipment.connection.address, **kwargs)
 
     def __getattr__(self, item: str) -> Any:  # noqa: ANN401
         """Get a pyvisa attribute."""
-        attr = getattr(self._resource, item)
-        if callable(attr):
-
-            def wrapper(*args: Any, **kwargs: Any) -> object:  # noqa: ANN401
-                return attr(*args, **kwargs)
-
-            return wrapper
-        return attr
+        return getattr(self._resource, item)
 
     def __setattr__(self, item: str, value: Any) -> None:  # pyright: ignore[reportImplicitOverride]  # noqa: ANN401
         """Set a pyvisa attribute."""
@@ -164,117 +124,55 @@ class PyVISA(Interface, backend=Backend.PyVISA):
         """Delete a pyvisa attribute."""
         delattr(self._resource, item)
 
-    @property
-    def resource(self) -> Resource | None:
-        """The PyVISA resource that is used for the connection.
-
-        This is the [Resource][pyvisa.resources.Resource]{:target="_blank"} instance
-        that would have been returned if you did the following
-
-        ```python
-        import pyvisa
-        rm = pyvisa.ResourceManager()
-        resource = rm.open_resource("ASRL3::INSTR")
-        ```
-        """
-        return self._resource
-
     def disconnect(self) -> None:  # pyright: ignore[reportImplicitOverride]
-        """Calls [pyvisa.resources.Resource.close][]{:target="_blank"}."""
+        """Calls [pyvisa.resources.Resource.close][]."""
         if self._resource is not None:
             self._resource.close()
             logger.debug("Disconnected from %s", self)
             self._resource = None
 
-    @staticmethod
-    def resource_manager(visa_library: str | VisaLibraryBase = "") -> ResourceManager:  # noqa: C901
-        """Returns the PyVISA Resource Manager.
 
-        Args:
-            visa_library: The library to use for PyVISA, for example,
+def _prepare_kwargs(props: dict[str, Any]) -> dict[str, Any]:  # noqa: C901
+    assert pyvisa is not None  # noqa: S101
 
-                * `@ivi` &mdash; PyVISA &ge; 1.11
-                * `@ni` &mdash; PyVISA &lt; 1.11
-                * `@py` &mdash; use [PyVISA-py](https://pyvisa.readthedocs.io/projects/pyvisa-py/en/stable/){:target="_blank"}
-                * `@sim` &mdash; use [PyVISA-sim](https://pyvisa.readthedocs.io/projects/pyvisa-sim/en/stable/){:target="_blank"}
+    # In PyVISA, Parity and StopBits must be integers
+    if "parity" in props:
+        parity = props["parity"]
+        if isinstance(parity, str):
+            parity = to_enum(parity, Parity, to_upper=True)
+        if isinstance(parity, Parity):
+            parity = parity.name.lower()
+            props["parity"] = to_enum(parity, pyvisa.constants.Parity)  # pyright: ignore[reportUnknownArgumentType, reportUnknownMemberType, reportAttributeAccessIssue]
 
-                If unspecified, the `PYVISA_LIBRARY` environment variable is used (if it exists).
-                This environment variable can be defined in a [configuration file][config-xml-example].
-                The default library is `@ivi` if not specified elsewhere.
+    if "stop_bits" in props:
+        stop_bits = props["stop_bits"]
+        if isinstance(stop_bits, str):
+            stop_bits = int(to_enum(stop_bits, StopBits, to_upper=True).value * 10)
+        elif isinstance(stop_bits, StopBits):
+            stop_bits = int(stop_bits.value * 10)
+        elif isinstance(stop_bits, (int, float)) and stop_bits < 10:  # noqa: PLR2004
+            stop_bits = int(stop_bits * 10)
+        props["stop_bits"] = to_enum(stop_bits, pyvisa.constants.StopBits)  # pyright: ignore[reportUnknownArgumentType, reportUnknownMemberType, reportAttributeAccessIssue]
 
-        Returns:
-            The PyVISA Resource Manager.
-        """
-        if pyvisa is None:
-            msg = "pyvisa is not installed, run: pip install pyvisa"  # type: ignore[unreachable]
-            raise ImportError(msg)
+    # PyVISA requires the read/write termination type to be str not bytes
+    def ensure_term(term: str | bytes) -> str:
+        return term.decode() if isinstance(term, bytes) else term
 
-        if not PyVISA._resource_classes:
-            for item in dir(pyvisa.resources):
-                if item.endswith("Instrument"):
-                    key = item[: -len("Instrument")]
-                    PyVISA._resource_classes[key] = getattr(pyvisa.resources, item)
-                elif item == "GPIBInterface":
-                    PyVISA._resource_classes["GPIB_INTFC"] = pyvisa.resources.GPIBInterface
-                elif item == "VXIBackplane":
-                    PyVISA._resource_classes["VXI_BACKPLANE"] = pyvisa.resources.VXIBackplane
-                elif item == "VXIMemory":
-                    PyVISA._resource_classes["VXI_MEMACC"] = pyvisa.resources.VXIMemory
-                elif item == "TCPIPSocket":
-                    PyVISA._resource_classes["TCPIP_SOCKET"] = pyvisa.resources.TCPIPSocket
-                elif item == "USBRaw":
-                    PyVISA._resource_classes["USB_RAW"] = pyvisa.resources.USBRaw
-                elif item == "PXIMemory":
-                    PyVISA._resource_classes["PXI_MEMACC"] = getattr(pyvisa.resources, item)
-            for item in ("COM", "ASRL", "LPT1", "ASRLCOM"):
-                PyVISA._resource_classes[item] = pyvisa.resources.SerialInstrument
+    # "termination" is a shortcut used by the MSL backend to set both
+    # write_termination and read_termination to the same value
+    rw_term = props.pop("termination", None)
+    r_term = props.pop("read_termination", rw_term)
+    w_term = props.pop("write_termination", rw_term)
+    if r_term is not None:
+        props["read_termination"] = ensure_term(r_term)
+    if w_term is not None:
+        props["write_termination"] = ensure_term(w_term)
 
-        return pyvisa.ResourceManager(visa_library)
+    # the "timeout" value is in seconds for MSL backend
+    # PyVISA uses a timeout in milliseconds
+    timeout: float | None = props.get("timeout")
+    if timeout and timeout < 600:  # noqa: PLR2004
+        # the value is probably in seconds
+        props["timeout"] = max(0, timeout * 1000)
 
-    @staticmethod
-    def resource_class(connection: Connection) -> type[Resource]:  # noqa: C901, PLR0911
-        """Get the PyVISA [Resource][pyvisa.resources.Resource]{:target="_blank"} class for the [Connection][].
-
-        Args:
-            connection: A [Connection][] instance.
-
-        Returns:
-            The type of PyVISA Resource class for `connection`.
-        """
-        address = str(connection.address)
-        if not address:
-            msg = "The Connection.address attribute has not been set"
-            raise ValueError(msg)
-
-        rm = PyVISA.resource_manager()
-        info = rm.resource_info(address, extended=True)
-        if info.resource_class is not None:
-            return rm._resource_classes[(info.interface_type, info.resource_class)]  # pyright: ignore[reportPrivateUsage]  # noqa: SLF001
-
-        # try to figure out the resource class...
-        a = str(address.upper())
-
-        if a.startswith("GPIB") and a.endswith("INTFC"):
-            return PyVISA._resource_classes["GPIB_INTFC"]
-
-        if a.startswith("VXI") and a.endswith("BACKPLANE"):
-            return PyVISA._resource_classes["VXI_BACKPLANE"]
-
-        if a.startswith("VXI") and a.endswith("MEMACC"):
-            return PyVISA._resource_classes["VXI_MEMACC"]
-
-        if a.startswith("TCPIP") and a.endswith("SOCKET"):
-            return PyVISA._resource_classes["TCPIP_SOCKET"]
-
-        if a.startswith("USB") and a.endswith("RAW"):
-            return PyVISA._resource_classes["USB_RAW"]
-
-        if a.startswith("PXI") and a.endswith("MEMACC"):
-            return PyVISA._resource_classes["PXI_MEMACC"]
-
-        for key, value in PyVISA._resource_classes.items():
-            if a.startswith(key):
-                return value
-
-        msg = f"Cannot find a PyVISA resource class for {address}"
-        raise ValueError(msg) from None
+    return props
---
+++ tests/test_pyvisa.py
@@ -0,0 +1,174 @@
+from __future__ import annotations
+
+import math
+import os
+from typing import TYPE_CHECKING
+
+import pytest
+from pyvisa.attributes import AttrVI_ATTR_TMO_VALUE
+from pyvisa.resources.tcpip import TCPIPSocket
+
+from msl.equipment import Backend, Connection, Parity, StopBits
+from msl.equipment.interfaces.pyvisa import _prepare_kwargs  # pyright: ignore[reportPrivateUsage]
+
+if TYPE_CHECKING:
+    from tests.conftest import TCPServer
+
+
+def teardown_module() -> None:
+    _ = os.environ.pop("PYVISA_LIBRARY", "")
+
+
+def setup_module() -> None:
+    os.environ["PYVISA_LIBRARY"] = "@py"
+
+
+@pytest.mark.parametrize("parity", ["even", "EVEN", "E", 2, Parity.EVEN])
+def test_prepare_kwargs_parity(parity: str | int) -> None:
+    # even parity in pyvisa has enum value 2
+    assert _prepare_kwargs({"parity": parity}) == {"parity": 2}
+
+
+@pytest.mark.parametrize("stop_bits", ["one_point_five", "ONE_POINT_FIVE", 1.5, 15, StopBits.ONE_POINT_FIVE])
+def test_prepare_kwargs_stop_bits(stop_bits: str | int) -> None:
+    # one_and_a_half stop_bits in pyvisa has enum value 15
+    assert _prepare_kwargs({"stop_bits": stop_bits}) == {"stop_bits": 15}
+
+
+@pytest.mark.parametrize(
+    ("term", "expected"), [("\r", "\r"), (b"\r", "\r"), ("\r\n", "\r\n"), (b"\r\n", "\r\n"), (b"abcdefg", "abcdefg")]
+)
+def test_prepare_kwargs_termination(term: str | bytes, expected: str) -> None:
+    assert _prepare_kwargs({"termination": term}) == {"read_termination": expected, "write_termination": expected}
+
+
+@pytest.mark.parametrize(
+    ("term", "expected"), [("\r", "\r"), (b"\r", "\r"), ("\r\n", "\r\n"), (b"\r\n", "\r\n"), (b"abcdefg", "abcdefg")]
+)
+def test_prepare_kwargs_read_termination(term: str | bytes, expected: str) -> None:
+    assert _prepare_kwargs({"read_termination": term}) == {"read_termination": expected}
+
+
+@pytest.mark.parametrize(
+    ("term", "expected"), [("\r", "\r"), (b"\r", "\r"), ("\r\n", "\r\n"), (b"\r\n", "\r\n"), (b"abcdefg", "abcdefg")]
+)
+def test_prepare_kwargs_write_termination(term: str | bytes, expected: str) -> None:
+    assert _prepare_kwargs({"write_termination": term}) == {"write_termination": expected}
+
+
+@pytest.mark.parametrize(
+    ("timeout", "expected"),
+    [
+        (-1.5, 0),
+        (0, 0),
+        (10, 10000),
+        (599, 599000),
+        (600, 600),
+        (5000, 5000),
+        (None, None),
+        (float("+inf"), float("+inf")),
+    ],
+)
+def test_prepare_kwargs_timeout(timeout: float | None, expected: float | None) -> None:
+    assert _prepare_kwargs({"timeout": timeout}) == {"timeout": expected}
+
+
+def test_timeout_and_termination_query(tcp_server: type[TCPServer]) -> None:
+    term = b"abc"
+    server = tcp_server(term=term)
+    server.start()
+
+    os.environ["PYVISA_LIBRARY"] = "@py"
+
+    address = f"TCPIP::{server.host}::{server.port}::SOCKET"
+    conn = Connection(
+        address,
+        manufacturer="A",
+        model="B",
+        serial="C",
+        backend=Backend.PyVISA,
+        termination=term,
+        timeout=10,
+    )
+
+    dev = conn.connect()
+    assert str(dev) == "PyVISA<A|B|C>"
+    assert repr(dev) == f"PyVISA<A|B|C at {address}>"
+
+    assert dev.timeout == 10000  # 10 seconds gets converted to 10000 ms
+    assert dev.write_termination == term.decode()
+    assert dev.read_termination == term.decode()
+
+    dev.timeout = 1234
+    dev.write_termination = "hello"
+    dev.read_termination = "goodbye"
+    assert dev.timeout == 1234
+    assert dev.write_termination == "hello"
+    assert dev.read_termination == "goodbye"
+
+    del dev.timeout
+    dev.write_termination = None
+    dev.read_termination = None
+    assert math.isinf(dev.timeout)
+    assert dev.write_termination is None
+    assert dev.read_termination is None
+
+    dev.timeout = 5000
+    dev.write_termination = term.decode()
+    dev.read_termination = term.decode()
+    assert dev.timeout == 5000
+    assert dev.write_termination == term.decode()
+    assert dev.read_termination == term.decode()
+
+    dev.timeout = None
+    assert math.isinf(dev.timeout)  # pyright: ignore[reportArgumentType]
+
+    assert dev.query("*IDN?") == "*IDN?"
+
+    dev.write("foo")
+    assert dev.read() == "foo"
+
+    dev.disconnect()
+    server.stop()
+
+
+def test_timeout_and_termination_1(tcp_server: type[TCPServer]) -> None:
+    term = b"abc"
+    server = tcp_server(term=term)
+    server.start()
+
+    conn = Connection(
+        f"TCPIP::{server.host}::{server.port}::SOCKET",
+        backend="PyVISA",
+        write_termination=b"abc",
+        read_termination="123",
+        timeout=1000,
+    )
+
+    dev = conn.connect()
+    assert dev.timeout == 1000  # >600 so does not get converted
+    assert dev.write_termination == "abc"
+    assert dev.read_termination == "123"
+
+    dev.disconnect()
+    server.stop()
+
+
+def test_timeout_and_termination_2(tcp_server: type[TCPServer]) -> None:
+    term = b"abc"
+    server = tcp_server(term=term)
+    server.start()
+
+    conn = Connection(
+        f"TCPIP::{server.host}::{server.port}::SOCKET",
+        backend="PyVISA",
+    )
+
+    dev = conn.connect()
+    assert dev.timeout == AttrVI_ATTR_TMO_VALUE.default
+    assert dev.write_termination == TCPIPSocket._write_termination  # pyright: ignore[reportPrivateUsage]  # noqa: SLF001
+    assert dev.read_termination == TCPIPSocket._read_termination  # pyright: ignore[reportPrivateUsage]  # noqa: SLF001
+    assert dev.read_termination is None
+
+    dev.disconnect()
+    server.stop()

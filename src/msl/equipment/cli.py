1aa42775dfc6c9eb61ac5a5b581d6f6006716d61
jborbely <joe.borbely@gmail.com>
2025-09-23 15:37:45 +1200

    fmt/lint cli.py

--- src/msl/equipment/cli.py
+++ src/msl/equipment/cli.py
@@ -2,173 +2,224 @@
 
 from __future__ import annotations
 
+import argparse
+import json
+import logging
+import os
+import sys
+from dataclasses import dataclass
+from enum import IntEnum, auto
+from threading import Thread
+from typing import TYPE_CHECKING
 
-def find_equipment(
+from serial.tools.list_ports import comports
+
+from .dns_service_discovery import find_lxi
+from .interfaces.gpib import find_listeners
+from .interfaces.prologix import find_prologix
+from .interfaces.vxi11 import find_vxi11
+from .utils import ipv4_addresses, logger
+
+if TYPE_CHECKING:
+    from collections.abc import ValuesView
+    from typing import ClassVar
+
+
+class _DeviceType(IntEnum):
+    ASRL = auto()
+    GPIB = auto()
+    PROLOGIX = auto()
+    LXI = auto()
+    VXI11 = auto()
+
+
+@dataclass
+class _Device:
+    type: _DeviceType
+    addresses: list[str]
+    description: str = ""
+    webserver: str = ""
+
+
+class _PrologixThread(Thread):
+    devices: ClassVar[dict[str, _Device]] = {}
+
+    def __init__(self, ips: list[str], timeout: float) -> None:
+        """Allows for capturing the return value from the target function."""
+
+        def function() -> None:
+            _PrologixThread.devices = {
+                k: _Device(type=_DeviceType.PROLOGIX, addresses=v.addresses, description=v.description)
+                for k, v in find_prologix(ip=ips, timeout=timeout).items()
+            }
+
+        super().__init__(target=function)
+
+
+class _LXIThread(Thread):
+    devices: ClassVar[dict[str, _Device]] = {}
+
+    def __init__(self, ips: list[str], timeout: float) -> None:
+        """Allows for capturing the return value from the target function."""
+
+        def function() -> None:
+            _LXIThread.devices = {
+                k: _Device(
+                    type=_DeviceType.LXI, addresses=v.addresses, description=v.description, webserver=v.webserver
+                )
+                for k, v in find_lxi(ip=ips, timeout=timeout).items()
+            }
+
+        super().__init__(target=function)
+
+
+class _VXI11Thread(Thread):
+    devices: ClassVar[dict[str, _Device]] = {}
+
+    def __init__(self, ips: list[str], timeout: float) -> None:
+        """Allows for capturing the return value from the target function."""
+
+        def function() -> None:
+            _VXI11Thread.devices = {
+                k: _Device(
+                    type=_DeviceType.VXI11, addresses=v.addresses, description=v.description, webserver=v.webserver
+                )
+                for k, v in find_vxi11(ip=ips, timeout=timeout).items()
+            }
+
+        super().__init__(target=function)
+
+
+def find_equipment(  # noqa: C901
     *, ip: list[str] | None = None, timeout: float = 2, gpib_library: str = "", include_sad: bool = True
-) -> ValuesView:
+) -> ValuesView[_Device]:
     """Returns information about equipment that are available.
 
-    :param ip:
-        The IP address(es) on the local computer to use to search for network
-        devices. If not specified, uses all network interfaces.
+    Args:
+        ip: The IP address(es) on the local computer to use to search for network devices.
+            If not specified, uses all network interfaces.
+        timeout: The maximum number of seconds to wait for a reply from a network device.
+        gpib_library: The path to a GPIB library file. The default file that is used is
+            platform dependent. If a GPIB library cannot be found, GPIB devices will not
+            be searched for.
+        include_sad: Whether to scan all secondary GPIB addresses.
 
-    :param timeout:
-        The maximum number of seconds to wait for a reply from a network device.
-
-    :param gpib_library:
-        The path to a GPIB library file. The default file that is used is
-        platform dependent. If a GPIB library cannot be found, GPIB devices
-        will not be searched for.
-
-    :param include_sad:
-        Whether to scan all secondary GPIB addresses.
-
-    :return: The information about the devices that were found.
+    Returns:
+        The information about the devices that were found.
     """
-    from threading import Thread
-    from msl.equipment.connection_gpib import find_listeners
-    from msl.equipment.connection_prologix import find_prologix
-    from msl.equipment.dns_service_discovery import find_lxi
-    from msl.equipment.utils import logger
-    from msl.equipment.vxi11 import find_vxi11
-    from serial.tools.list_ports import comports
-
-    class NetworkThread(Thread):
-        def __init__(self, target):
-            """Allows for capturing the return value from the target function."""
-            self.devices = {}
-
-            def function():
-                self.devices = target(ip=ip, timeout=timeout)
-
-            super(NetworkThread, self).__init__(target=function)
-
     logger.debug("start finding devices")
-    devices = {}
+    num_found = 0
+    devices: dict[str, _Device] = {}
 
-    threads = [
-        NetworkThread(target=find_lxi),
-        NetworkThread(target=find_vxi11),
-        NetworkThread(target=find_prologix),
+    ips = ip if ip is not None else list(ipv4_addresses())
+    threads: list[_PrologixThread | _LXIThread | _VXI11Thread] = [
+        _PrologixThread(ips, timeout),
+        _LXIThread(ips, timeout),
+        _VXI11Thread(ips, timeout),
     ]
     for thread in threads:
         thread.start()
 
-    num_found = 0
-
     logger.debug("find ASRL ports")
-    for port, desc, _ in sorted(comports()):
+    for info in sorted(comports()):
         num_found += 1
-        addresses = []
-        if port.startswith("COM"):
-            addresses.append(port)
-        elif port.startswith("/dev/"):
-            addresses.append(f"ASRL{port}")
-        devices[port] = {"type": "Serial", "addresses": addresses, "description": desc}
+        addresses: list[str] = []
+        if info.device.startswith("COM"):
+            addresses.append(info.device)
+        elif info.device.startswith("/dev"):
+            addresses.append(f"ASRL{info.device}")
+        devices[info.device] = _Device(type=_DeviceType.ASRL, addresses=addresses, description=info.description)
 
-    Config.GPIB_LIBRARY = gpib_library
+    if gpib_library:
+        os.environ["GPIB_LIBRARY"] = gpib_library
+
     gpib = find_listeners(include_sad=include_sad)
     if gpib:
         num_found += len(gpib)
-        devices["gpib"] = {"type": "GPIB", "addresses": gpib, "description": ""}
+        devices["GPIB"] = _Device(type=_DeviceType.GPIB, addresses=gpib)
 
     for thread in threads:
         thread.join()
+
     for thread in threads:
-        for ipv4, device in thread.devices.items():
-            description = device.get("description", "Unknown device")
-            if ipv4 not in devices:
+        for name, device in thread.devices.items():
+            if name not in devices:
                 num_found += 1
-                devices[ipv4] = {
-                    "type": "Network",
-                    "addresses": device["addresses"],
-                    "description": description,
-                }
-                if not description.startswith("Prologix"):
-                    # Prologix ENET-GPIB does not have a webserver
-                    devices[ipv4]["webserver"] = device["webserver"]
-            else:
-                if devices[ipv4]["description"] == "Unknown device" and description != "Unknown device":
-                    devices[ipv4]["description"] = description
-                for address in device["addresses"]:
-                    if address not in devices[ipv4]["addresses"]:
-                        devices[ipv4]["addresses"].append(address)
+                devices[name] = device
 
     logger.debug("found %d devices", num_found)
     return devices.values()
 
 
-def _print_stdout(equipment: ValuesView) -> None:
+def _print_stdout(found: ValuesView[_Device]) -> None:
     """Print a summary of all equipment that are available to connect to."""
-    devices = sorted(equipment, key=lambda v: v["description"])
-    types = sorted(set(d["type"] for d in devices))
+    devices = sorted(found, key=lambda v: v.description)
+    types = sorted({d.type for d in devices})
     for typ in types:
-        print(f"{typ} Devices")
+        print(f"{typ} Devices")  # noqa: T201
         for device in devices:
-            if device["type"] != typ:
+            if device.type != typ:
                 continue
-            if typ == "GPIB":
-                print("  " + "\n  ".join(device["addresses"]))
-            elif typ == "Serial":
-                print(f"  {device['addresses'][0]} [{device['description']}]")
-            elif typ == "Network":
-                print(f"  {device['description']}")
-                if "webserver" in device:
-                    print(f"    webserver {device['webserver']}")
-                if device["addresses"]:
-                    print(f"    " + "\n    ".join(sorted(device["addresses"])))
+            if typ == _DeviceType.GPIB:
+                print("  " + "\n  ".join(device.addresses))  # noqa: T201
+            elif typ == _DeviceType.ASRL:
+                print(f"  {device.addresses[0]} [{device.description}]")  # noqa: T201
+            elif typ == _DeviceType.PROLOGIX | _DeviceType.LXI | _DeviceType.VXI11:
+                print(f"  {device.description}")  # noqa: T201
+                if device.webserver:
+                    print(f"    webserver {device.webserver}")  # noqa: T201
+                if device.addresses:
+                    print("    " + "\n    ".join(sorted(device.addresses)))  # noqa: T201
 
 
-def _find_cli() -> None:
+def cli() -> None:
     """Console script entry point to find equipment."""
-    import argparse
-    import json
-    import sys
-
     parser = argparse.ArgumentParser(
         add_help=False,
         description="Find equipment that can be connected to.",
         formatter_class=argparse.RawTextHelpFormatter,
     )
-    parser.add_argument(
+    _ = parser.add_argument(
         "-h",
         "--help",
         action="help",
         help="Show this help message and exit.",
         default=argparse.SUPPRESS,
     )
-    parser.add_argument(
+    _ = parser.add_argument(
         "--ip",
         nargs="*",
-        help="The IP address(es) on the local computer to search for network\n"
-        "devices. If not specified, uses all network interfaces.",
+        help=(
+            "The IP address(es) on the local computer to search for network\n"
+            "devices. If not specified, uses all network interfaces."
+        ),
     )
-    parser.add_argument(
+    _ = parser.add_argument(
         "-t",
         "--timeout",
         type=float,
         default=2,
-        help="Maximum number of seconds to wait for a reply from a network\ndevice. Default is 2 seconds.",
+        help=("Maximum number of seconds to wait for a reply from a network\ndevice. Default is 2 seconds."),
     )
-    parser.add_argument(
+    _ = parser.add_argument(
         "-g",
         "--gpib-library",
         default="",
-        help="The path to a GPIB library file. The default file that is used\n"
-        "is platform dependent. If a GPIB library cannot be found, GPIB\n"
-        "devices will not be searched for.",
+        help=(
+            "The path to a GPIB library file. The default file that is used\n"
+            "is platform dependent. If a GPIB library cannot be found, GPIB\n"
+            "devices will not be searched for."
+        ),
     )
-    parser.add_argument("--debug", action="store_true", default=False, help="Whether to show DEBUG log messages.")
-    parser.add_argument(
+    _ = parser.add_argument("--debug", action="store_true", default=False, help="Whether to show DEBUG log messages.")
+    _ = parser.add_argument(
         "--ignore-sad", action="store_true", default=False, help="Do not scan for secondary GPIB addresses."
     )
-    parser.add_argument("--json", action="store_true", default=False, help="Print the results as a JSON string.")
+    _ = parser.add_argument("--json", action="store_true", default=False, help="Print the results as a JSON string.")
 
     parsed = parser.parse_args(sys.argv[1:])
 
     if parsed.debug:
-        import logging
-
         logging.basicConfig(
             level=logging.DEBUG,
             format="%(asctime)s.%(msecs)03d [%(levelname)s] %(name)s %(message)s",
@@ -183,6 +234,6 @@ def _find_cli() -> None:
     )
 
     if parsed.json:
-        print(json.dumps(list(equipment), indent=2))
+        print(json.dumps(list(equipment), indent=2))  # noqa: T201
     else:
         _print_stdout(equipment)

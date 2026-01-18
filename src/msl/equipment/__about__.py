4d69cdbf65b2a5f11374c61c9ed47f678ee92005
jborbely <joe.borbely@gmail.com>
2026-01-19 12:16:26 +1300

    update copyright year to 2026

--- src/msl/equipment/__about__.py
+++ src/msl/equipment/__about__.py
@@ -6,4 +6,4 @@ from ._version import __version__, version_tuple
 
 __all__: list[str] = ["__version__", "version_tuple"]
 __author__: str = "Measurement Standards Laboratory of New Zealand"
-__copyright__: str = f"\xa9 2017 - 2025, {__author__}"
+__copyright__: str = f"\xa9 2017 - 2026, {__author__}"

eebf96d9c04b174d0d4438f1b66da0f8b572fa60
jborbely <joe.borbely@gmail.com>
2026-05-09 12:33:57 +1200

    docs: update arithmatex.js and use unpkg in `extra_javascript`

--- docs/javascripts/arithmatex.js
+++ docs/javascripts/arithmatex.js
@@ -10,3 +10,10 @@ window.MathJax = {
     processHtmlClass: "arithmatex"
   }
 };
+
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  // MathJax.typesetPromise() // The piecewise equation in cvd_equation.md does not render properly when enabled
+})
